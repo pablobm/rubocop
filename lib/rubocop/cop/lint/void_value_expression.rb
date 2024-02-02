c90b9f4196853f7a9906decb7903f77ba4ce0ddd
Pablo Brasero <pablo@pablobm.com>
2024-02-02 11:50:43 +0000

    Lint

--- lib/rubocop/cop/lint/void_value_expression.rb
+++ lib/rubocop/cop/lint/void_value_expression.rb
@@ -5,16 +5,11 @@ module RuboCop
     module Lint
       SKIPPABLE_STATEMENTS = %i[kwbegin if while begin rescue ensure resbody].freeze
       LIMIT_STATEMENTS = %i[def defs block case when case_match in_pattern].freeze
-      CONTROL_STATEMENTS = %i[or and]
+      CONTROL_STATEMENTS = %i[or and].freeze
 
       class VoidValueExpression < Base
         def on_return(return_node)
-          relevant_ancestry =
-            return_node
-            .ancestors
-            .take_while { |n| !LIMIT_STATEMENTS.include?(n.type) }
-            .reject { |n| SKIPPABLE_STATEMENTS.include?(n.type) }
-
+          relevant_ancestry = filter_relevant_ancestry(return_node)
           parent_node = relevant_ancestry.first
 
           return unless parent_node
@@ -28,6 +23,13 @@ module RuboCop
 
         def in_control_statement_but_not_first?(node, parent)
           CONTROL_STATEMENTS.include?(parent.type) && parent.children.first != node
+        end
+
+        def filter_relevant_ancestry(node)
+          node
+            .ancestors
+            .take_while { |n| !LIMIT_STATEMENTS.include?(n.type) }
+            .reject { |n| SKIPPABLE_STATEMENTS.include?(n.type) }
         end
       end
     end
